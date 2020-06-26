f0c21bba60ae3a5ae6fc6c4cc335bc99fb8a048d
Luke Stanley <1015748@student.sae.edu.au>
2020-06-26 15:34:11 +1000

    Glide bug fixes
    jumping out of glide cancels glide, need to un click and reclick to reactivate

--- Unyeilding Diamond/Assets/Scripts/PlayerController.cs
+++ Unyeilding Diamond/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //basic movement stuff
     public float speed;
     public float jumpForce;
     private float moveInput;
 
     private Rigidbody2D rb;
     private bool facingRight = true;
-
+    //jump checks
     private bool isGrounded;
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatisGround;
     private int extraJumps;
     public int extrajumpsMax;
-
+    //dash checks
     public bool hasDash = false;
     public float dashSpeed;
     public float dashTime;
-
+    //glide checks
     public bool hasGlide = false;
     public float glidePower;
     public float originalGravity;
@@ -55,36 +56,49 @@ public class PlayerController : MonoBehaviour
      void Update()
     {
         //jump
-        if (isGrounded == true)
+        if (isGrounded)
         {
+            //reset jumps
             extraJumps = extrajumpsMax;
+            //reset gravity
+            rb.gravityScale = originalGravity;
         }
         if (Input.GetButtonDown("Jump") && extraJumps > 0)
         {
+            //diable glide
+            rb.gravityScale = originalGravity;
+            //jump
             rb.velocity = Vector2.up * jumpForce;
+            //minus one jump
             extraJumps--;
-        }else if (Input.GetButtonDown("Jump") && extraJumps == 0 && isGrounded == true)
+        }else if (Input.GetButtonDown("Jump") && extraJumps == 0 && isGrounded)
         {
+            //jump from ground without reducing extra jumps
             rb.velocity = Vector2.up * jumpForce;
         }
         //dash
-        if(hasDash == true && Input.GetMouseButtonDown(0))
+        if(hasDash && Input.GetMouseButtonDown(0))
         {
             StartCoroutine("Dash");
         }
         //glide
-        if(hasGlide == true && Input.GetMouseButtonDown(1) && rb.velocity.y <= 0)
+        if (hasGlide && Input.GetMouseButtonDown(1) && rb.velocity.y <= 0 && !isGrounded)
         {
+            //reduce gravity when falling
             rb.gravityScale /= glidePower;
-        }else if(hasGlide == true && Input.GetMouseButtonDown(1) && rb.velocity.y > 0)
+        }else if(hasGlide && Input.GetMouseButtonDown(1) && rb.velocity.y > 0)
         {
+            //stop all vertical movement
             rb.velocity = new Vector2(moveInput * speed, 0);
+            //reduce gravity
             rb.gravityScale /= glidePower;
         }
-        if (hasGlide == true && Input.GetMouseButtonUp(1))
+        if (hasGlide && Input.GetMouseButtonUp(1))
         {
+            //reset gravity
             rb.gravityScale = originalGravity;
         }
+
     }
 
     IEnumerator Dash()
