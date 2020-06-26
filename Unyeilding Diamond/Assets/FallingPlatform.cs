7c22ab6a0a6ad3e1215b6b103ce72d04602beea7
Luke Stanley <1015748@student.sae.edu.au>
2020-06-26 16:34:46 +1000

    Added Falling Platform

---
+++ Unyeilding Diamond/Assets/FallingPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    public float crumbleTime;
+    public Collider2D myCollider;
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>().gravityScale;
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            StartCoroutine("Collapse");
+        }
+    }
+
+    IEnumerator Collapse()
+    {
+        //crumble animation
+        yield return new WaitForSeconds(crumbleTime);
+        myCollider.enabled = false;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        rb.velocity = Vector2.down * .001f;
+        Debug.Log("falling");
+
+    }
+}
