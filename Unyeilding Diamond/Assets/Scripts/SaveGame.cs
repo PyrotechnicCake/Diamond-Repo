c7963d46ca0f1c64f300e830dc401817c0ca1012
Luke Stanley <1015748@student.sae.edu.au>
2020-07-13 10:41:11 +1000

    Began SaveGame Script
    May need lots of changes

---
+++ Unyeilding Diamond/Assets/Scripts/SaveGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveGame : MonoBehaviour
+{
+    public GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Save()
+    {
+        //save level
+        //save player position
+        PlayerPrefs.SetFloat("PlayerX", player.transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", player.transform.position.y);
+        PlayerPrefs.SetFloat("PlayerZ", player.transform.position.z);
+        //save player stats
+        PlayerPrefs.SetInt("hasJump", (player.GetComponent<PlayerController>().hasJump ? 1 : 0));
+        PlayerPrefs.SetInt("hasDash", (player.GetComponent<PlayerController>().hasDash ? 1 : 0));
+        PlayerPrefs.SetInt("hasGlide", (player.GetComponent<PlayerController>().hasGlide ? 1 : 0));
+        PlayerPrefs.SetInt("extraJumps", player.GetComponent<PlayerController>().extrajumpsMax);
+    }
+}
