092d88d224aa96c7607db58c75d42c09bff34044
Luke Stanley <1015748@student.sae.edu.au>
2020-07-22 14:30:35 +1000

    Music Plays throughout
    still needs work

--- Unyeilding Diamond/Assets/Scripts/SoundManager.cs
+++ Unyeilding Diamond/Assets/Scripts/SoundManager.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
+    public AudioSource source;
+    public AudioClip titleMusic;
+    public AudioClip inGameMusicOne;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoad;
+        source = GetComponent<AudioSource>();
+        source.PlayOneShot(titleMusic);
+    }
+
+    void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+       if(scene.buildIndex == 2)
+        {
+            source.Stop();
+            source.PlayOneShot(inGameMusicOne);
+        }
     }
 
     // Update is called once per frame
