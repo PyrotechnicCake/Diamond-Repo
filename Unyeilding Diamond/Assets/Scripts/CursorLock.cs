7955f422a250a7dc6c72a49c65c0cede04608a38
Rohith Maddali <1015083@student.sae.edu.au>
2020-08-13 03:39:35 +1000

    Added an interesting subtle effect

---
+++ Unyeilding Diamond/Assets/Scripts/CursorLock.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLock : MonoBehaviour
+{
+    private Coroutine co_HideCursor;
+
+    void Update()
+    {
+        if (Input.GetAxis("Mouse X") == 0 && (Input.GetAxis("Mouse Y") == 0))
+        {
+            if (co_HideCursor == null)
+            {
+                co_HideCursor = StartCoroutine(HideCursor());
+            }
+        }
+        else
+        {
+            if (co_HideCursor != null)
+            {
+                StopCoroutine(co_HideCursor);
+                co_HideCursor = null;
+                Cursor.visible = true;
+            }
+        }
+    }
+
+    private IEnumerator HideCursor()
+    {
+        yield return new WaitForSeconds(3);
+        Cursor.visible = false;
+    }
+}
+
+
