85348c28307d46d4a04e683e45296c4c84c746a8
Rohith Maddali <1015083@student.sae.edu.au>
2020-08-14 00:18:16 +1000

    Added a custom mouse cursor (Just a test can be deleted if not nice)

---
+++ Unyeilding Diamond/Assets/Scripts/FollowCursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+    }
+}
