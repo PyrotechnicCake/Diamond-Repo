b422ec67d4ad428825a401a565587ba3e4ad0d5e
Rohith Maddali <1015083@student.sae.edu.au>
2020-08-22 14:36:03 +1000

    Added subtle fade in animation to buttons in main menu, titlescreen and end scene

---
+++ Unyeilding Diamond/Assets/Scripts/ButtonFadeIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFadeIn : MonoBehaviour
+{
+    public Animator anim;
+    public int t;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        StartCoroutine(ButtonFade());
+    }
+
+    IEnumerator ButtonFade()
+    {
+        yield return new WaitForSeconds(t);
+        anim.SetTrigger("Play");
+        anim.SetTrigger("Menu");
+        anim.SetTrigger("End");
+    }
+
+}
